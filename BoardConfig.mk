8fc96d683d031f294a5170b72178a62e00380de8
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2024-09-08 15:54:55 +0800

    i like busybox

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,6 @@ TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
+TW_USE_TOOLBOX := false
 TWRP_INCLUDE_LOGCAT := true
 TWRP_EVENT_LOGGING := true
